3763c7ceb3e8a364c40af1840087c95eed02bf91
YAO <425045233@qq.com>
2023-05-23 13:03:31 +0800

    lab02 readme.md

--- LAB/lab10/omp_apps.h
+++ LAB/lab10/omp_apps.h
@@ -17,16 +17,16 @@
 
 // for web server
 char *compute_dotp(int);
-char *image_proc(const char*);
+char *image_proc(const char *);
 
-void v_add_naive(double* x, double* y, double* z);
-void v_add_optimized_adjacent(double* x, double* y, double* z);
-void v_add_optimized_chunks(double* x, double* y, double* z);
-double dotp_naive(double* x, double* y, int arr_size);
-double dotp_manual_optimized(double* x, double* y, int arr_size);
-double dotp_reduction_optimized(double* x, double* y, int arr_size);
+void v_add_naive(double *x, double *y, double *z);
+void v_add_optimized_adjacent(double *x, double *y, double *z);
+void v_add_optimized_chunks(double *x, double *y, double *z);
+double dotp_naive(double *x, double *y, int arr_size);
+double dotp_manual_optimized(double *x, double *y, int arr_size);
+double dotp_reduction_optimized(double *x, double *y, int arr_size);
 
-double* gen_array(int n);
-int verify(double* x, double* y, void(*funct)(double *x, double *y, double *z));
+double *gen_array(int n);
+int verify(double *x, double *y, void (*funct)(double *x, double *y, double *z));
 
 #endif
