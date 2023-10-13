51ce3ca7e52b1b02f8c6f9a7ab92203531272e1a
YaoTengqi <425045233@qq.com>
2023-10-13 11:23:31 +0800

    task 2 passed, make my_numc Cython library

--- PROJECT/PROJ4/setup.py
+++ PROJECT/PROJ4/setup.py
@@ -6,7 +6,17 @@ def main():
     LDFLAGS = ['-fopenmp']
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
-    # TODO: YOUR CODE HERE
+    module = Extension('numc',
+                       include_dirs=['/home/ytq/codeField/CS61C/PROJECT/PROJ4'],
+                       sources=['numc.c']
+                       )
+    
+    setup(
+        name='My numc',
+        version='1.0',
+        description='This is my numc Cython library',
+        ext_modules=[module]
+    )
 
 if __name__ == "__main__":
     main()
